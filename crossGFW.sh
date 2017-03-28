4d87541de69424d2efee4909aa1c6e4bbbc4b075
root <root@vultr.guest>
2017-03-28 13:02:11 +0000

    add iptables

--- crossGFW.sh
+++ crossGFW.sh
@@ -2,5 +2,6 @@
 
 service serverSpeeder restart
 ssserver -c /etc/shadowsocks.json --log-file /tmp/ss.log -d start
-iptatbles -F
+#iptatbles -F
+iptables -A INPUT -p tcp --dport 8388 -j ACCEPT
 gollum &
