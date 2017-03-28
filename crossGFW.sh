c0bf98d8a3a07afcf0c9965d2a61b5751ba1b8d9
root <root@instance-1.c.silver-script-162707.internal>
2017-03-28 13:09:03 +0000

    modify crossGFW.sh restore_config.sh

--- crossGFW.sh
+++ crossGFW.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-service serverSpeeder restart
-ssserver -c /etc/shadowsocks.json --log-file /tmp/ss.log -d start
+#service serverSpeeder restart
+#ssserver -c /etc/shadowsocks.json --log-file /tmp/ss.log -d start
+yum install wget git net-tools
+wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-go.sh
+chmod +x shadowsocks-go.sh
+./shadowsocks-go.sh 2>&1 | tee shadowsocks-go.log
 #iptatbles -F
-iptables -A INPUT -p tcp --dport 8388 -j ACCEPT
-gollum &
+#iptables -A INPUT -p tcp --dport 8388 -j ACCEPT
+#gollum &
